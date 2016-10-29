8a28466559f494ca880cc5b09abfb6bfcfbb05df
Gondal <gondals@pun-gondals-m.ds.ad.adp.com>
2016-10-29 18:04:29 +0530

    XMl based configuration for DI . Have multiple configuration files.

---
+++ src/main/java/com/sandy/spring/di/xml/Department.java
@@ -0,0 +1,36 @@
+package com.sandy.spring.di.xml;
+
+public class Department {
+
+    private int id;
+    private String name;
+
+    public Department() {
+        this.id = 2000;
+        this.name = "Software";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/sandy/spring/di/xml/MainClass.java
+++ src/main/java/com/sandy/spring/di/xml/MainClass.java
@@ -8,9 +8,11 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello Spring ... XML Based configuration for DI");
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("XMLConfig.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("XMLConfig1.xml", "XMLConfig2.xml");
         Employee employee = (Employee) context.getBean("employee");
+        Department department = (Department) context.getBean("department");
         System.out.println(employee);
+        System.out.println(department);
     }
 
 }
