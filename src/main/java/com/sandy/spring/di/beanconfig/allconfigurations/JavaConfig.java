bcf4f6385d66ac669aacaf7d6da7e76dd5c0c7d4
Gondal <gondals@pun-gondals-m.ds.ad.adp.com>
2016-10-31 10:48:11 +0530

    Imported XML config into JavaConfig

--- src/main/java/com/sandy/spring/di/beanconfig/allconfigurations/JavaConfig.java
+++ src/main/java/com/sandy/spring/di/beanconfig/allconfigurations/JavaConfig.java
@@ -2,8 +2,10 @@ package com.sandy.spring.di.beanconfig.allconfigurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ImportResource("classpath:allConfigurations.xml")
 public class JavaConfig {
 
     @Bean
--- src/main/java/com/sandy/spring/di/beanconfig/allconfigurations/MainClass.java
+++ src/main/java/com/sandy/spring/di/beanconfig/allconfigurations/MainClass.java
@@ -1,13 +1,13 @@
 package com.sandy.spring.di.beanconfig.allconfigurations;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Hello Spring. This is mix of all configurations for a bean.");
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("allConfigurations.xml");
-        System.out.println(classPathXmlApplicationContext.getBean(Car.class));
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
+        System.out.println(annotationConfigApplicationContext.getBean(Car.class));
     }
 
 }
